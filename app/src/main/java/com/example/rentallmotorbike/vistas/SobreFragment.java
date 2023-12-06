1f93e30d0cf175f1d176db56e72d39cf1d739538
Lucas <2201118@my.ipleiria.pt>
2023-12-06 12:50:58 +0000

    Create SobreFragment.java

---
+++ app/src/main/java/com/example/rentallmotorbike/vistas/SobreFragment.java
@@ -0,0 +1,36 @@
+package com.example.rentallmotorbike.vistas;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.rentallmotorbike.R;
+
+public class SobreFragment extends Fragment {
+
+    public SobreFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view = inflater.inflate(R.layout.fragment_sobre, container, false);
+       ImageView imgCapa = view.findViewById(R.id.imageView3);
+
+        Glide.with(this)
+                .load(SobreFragment.this)
+                .placeholder(R.drawable.logo)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgCapa);
+
+        return view;
+    }
+}
