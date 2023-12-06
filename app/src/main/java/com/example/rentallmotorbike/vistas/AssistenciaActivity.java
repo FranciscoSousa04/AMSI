d587b6cb50a6676a657d182f966602baf6a3d27a
lucas <2201118@my.ipleiria.pt>
2023-12-06 12:46:19 +0000

    reservas e assistencia

---
+++ app/src/main/java/com/example/rentallmotorbike/vistas/AssistenciaActivity.java
@@ -0,0 +1,16 @@
+package com.example.rentallmotorbike.vistas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rentallmotorbike.R;
+
+public class AssistenciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assistencia);
+    }
+}
--- app/src/main/java/com/example/rentallmotorbike/vistas/DetalhesReservaActivity.java
+++ app/src/main/java/com/example/rentallmotorbike/vistas/DetalhesReservaActivity.java
@@ -3,6 +3,7 @@ package com.example.rentallmotorbike.vistas;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +14,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.rentallmotorbike.R;
 import com.example.rentallmotorbike.modelo.Reserva;
 import com.example.rentallmotorbike.modelo.SingletonGestorMotociclos;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 
 public class DetalhesReservaActivity extends AppCompatActivity {
 
@@ -70,6 +77,62 @@ public class DetalhesReservaActivity extends AppCompatActivity {
 
     private void carregarVeiculo() {
         Resources resources = getResources();
+        String nome = String.format("Detalhes: ", reserva.getMarca() + "" + reserva.getModelo());
+        setTitle(nome);
+        etMarca.setText(reserva.getMarca());
+        etModelo.setText(reserva.getModelo());
+        etseguro.setText(reserva.getModelo());
+        tvLocalL.setText(reserva.getLocalizacao_levantamento());
+        tvDataL.setText(reserva.getData_inicio() + "");
+        tvLocalD.setText(reserva.getLocalizacao_devolucao());
+        tvDataD.setText(reserva.getData_fim() + "");
+        tvPreco.setText(reserva.getPreco() + "€");
+        tvMatricula.setText(reserva.getMatricula());
+        //imgCapa.setImageBitmap(makeqr(reserva.getId() + ""));
+
+        String dateFormat1 = reserva.getData_inicio();
+        String dateFormat2 = reserva.getData_fim();
+        SimpleDateFormat format = new SimpleDateFormat("d/MM/yyyy");
+        Date date = new Date();
+        try {
+
+            Date dataInicio = format.parse(dateFormat1);
+            Date dataFim = format.parse(dateFormat2);
+
+            if(date.compareTo(dataInicio) >= 0 && date.compareTo(dataFim) <= 0 ){
+                btnPedirAssistencia.setVisibility(View.VISIBLE);
+            }
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
+
+    //private Bitmap makeqr(String id) {
+
+      //  QRGEncoder qrgEncoder = new QRGEncoder(id , null, QRGContents.Type.TEXT, 500);
+       // qrgEncoder.setColorBlack(Color.WHITE);
+        //qrgEncoder.setColorWhite(Color.BLACK);
+        //try {
+          //  Bitmap bitmap = qrgEncoder.getBitmap();
+            //return bitmap;
+        //} catch (Exception e) {
+          //  e.printStackTrace();
+           // return null;
+        //}
+
+    //}
+
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        if(reserva != null){
+            //getMenuInflater().inflate(R.menu.menu_detalhes_moto, menu);
+            //return super.onCreateOptionsMenu(menu);
+        }
+        return false;
+    }
+
+
 }
