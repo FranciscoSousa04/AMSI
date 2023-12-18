dceec934f7778b28c9988a8652f70ef0f468ff46
JLucas220 <73337859+JLucas220@users.noreply.github.com>
2023-12-18 13:26:54 +0000

    json

---
+++ app/src/main/java/com/example/rentallmotorbike/utils/ExtraJsonParser.java
@@ -0,0 +1,40 @@
+package com.example.rentallmotorbike.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.example.rentallmotorbike.modelo.Extras;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class ExtraJsonParser {
+
+    public static ArrayList<Extras> parseJsonExtras(JSONArray response) {
+        ArrayList<Extras> extras = new ArrayList<>();
+        try {
+            for (int i = 0; i < response.length(); i++) {
+                JSONObject rota = (JSONObject) response.get(i);
+                int id = rota.getInt("id_extra");
+                String nome = rota.getString("descricao");
+                float preco = BigDecimal.valueOf(rota.getDouble("preco")).floatValue();
+                Extras auxExtra = new Extras(id, nome, preco);
+                extras.add(auxExtra);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return extras;
+    }
+
+    public static Boolean isConnectionInternet(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+}
---
+++ app/src/main/java/com/example/rentallmotorbike/utils/PerfilJsonParser.java
@@ -0,0 +1,61 @@
+package com.example.rentallmotorbike.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.example.rentallmotorbike.modelo.Perfil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class PerfilJsonParser {
+
+    public static ArrayList<Perfil> parseJsonDadosPessoais(JSONArray response) {
+        ArrayList<Perfil> dadosPessoais = new ArrayList<>();
+        try {
+            for (int i = 0; i < response.length(); i++) {
+                JSONObject dados = (JSONObject) response.get(i);
+                int id = dados.getInt("id_profile");
+                String nome = dados.getString("nome");
+                String apelido = dados.getString("apelido");
+                //String imgPerfil = dados.getString("imgPerfil");
+                int telemovel = dados.getInt("telemovel");
+                int nif = dados.getInt("nif");
+                int nrcarta=dados.getInt("nr_carta_conducao");
+                Perfil auxDadosPessoal = new Perfil(id, nome, apelido, /*imgPerfil,*/ telemovel, nif,nrcarta);
+                dadosPessoais.add(auxDadosPessoal);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return dadosPessoais;
+    }
+
+    public static Perfil parseJsonDadosPessoal(String response) {
+        Perfil auxDadosPessoal = null;
+        try {
+            JSONObject dados = new JSONObject(response);
+            int id = dados.getInt("id_profile");
+            String nome = dados.getString("nome");
+            String apelido = dados.getString("apelido");
+            //String imgPerfil = dados.getString("imgPerfil");
+            int telemovel = dados.getInt("telemovel");
+            int nif = dados.getInt("nif");
+            int nrcarta=dados.getInt("nr_carta_conducao");
+            auxDadosPessoal = new Perfil(id, nome, apelido, /*imgPerfil,*/ telemovel, nif,nrcarta);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return auxDadosPessoal;
+    }
+
+    public static Boolean isConnectionInternet(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+}
