2f6b3c89a5804c6bebab4f57d853d89372437f0e
FranciscoSousa <2220852@my.ipleiria.pt>
2023-12-19 12:15:56 +0000

    Reserva Layout

---
+++ app/src/main/java/com/example/rentallmotorbike/vistas/ReservaMotocicloActivity.java
@@ -0,0 +1,19 @@
+package com.example.rentallmotorbike.vistas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rentallmotorbike.R;
+
+public class ReservaMotocicloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserva_motociclo);
+
+
+
+    }
+}
