2c3cf553f1e20826d21344240c1b0c2ce1986418
Francisco <2220852@my.ipleiria.pt>
2023-12-21 01:50:03 +0000

    extraslistener

---
+++ app/src/main/java/com/example/rentallmotorbike/listeners/ExtrasListener.java
@@ -0,0 +1,9 @@
+package com.example.rentallmotorbike.listeners;
+
+import com.example.rentallmotorbike.modelo.Extras;
+
+import java.util.ArrayList;
+
+public interface ExtrasListener {
+    void onRefreshListaExtras(ArrayList<Extras> extras);
+}
