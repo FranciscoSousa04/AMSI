46af29d27e10b45e1a3079174d23968d783f42e6
LOPERSON <2201104@my.ipleiria.pt>
2023-12-27 01:30:41 +0000

    Gestor
    esta um erro por corrigir na toolbar no menumaingestor

---
+++ app/src/main/java/com/example/rentallmotorbike/vistas/ListaReservasGestorFragment.java
@@ -0,0 +1,62 @@
+package com.example.rentallmotorbike.vistas;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.rentallmotorbike.R;
+import com.example.rentallmotorbike.adaptadores.ListaReservasAdaptador;
+import com.example.rentallmotorbike.listeners.ReservasListener;
+import com.example.rentallmotorbike.modelo.Reserva;
+import com.example.rentallmotorbike.modelo.SingletonGestorMotociclos;
+
+import java.util.ArrayList;
+
+
+public class ListaReservasGestorFragment extends Fragment implements ReservasListener {
+
+    private ListView lvGReservas;
+
+    public static final int DETALHES = 2;
+
+    public ListaReservasGestorFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lista_reservas_gestor,container,false);
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("user_info", MODE_PRIVATE);
+        int id = sharedPreferences.getInt("id",-1);
+        lvGReservas = view.findViewById(R.id.lvGReservas);
+        SingletonGestorMotociclos.getInstance(getContext()).setReservasListener(this);
+        SingletonGestorMotociclos.getInstance(getContext()).getALLReservasAPI(getContext());
+
+        lvGReservas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getContext(), DetalhesReservaActivity.class);
+                intent.putExtra(DetalhesReservaActivity.IDRESERVA, (int) id);
+                startActivityForResult(intent, DETALHES);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onRefreshListaReservas(ArrayList<Reserva> reservas) {
+        if (reservas != null)
+            lvGReservas.setAdapter(new ListaReservasAdaptador(getContext(),reservas));
+    }
+}
---
+++ app/src/main/java/com/example/rentallmotorbike/vistas/MenuMainGestorActivity.java
@@ -0,0 +1,140 @@
+package com.example.rentallmotorbike.vistas;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.rentallmotorbike.R;
+import com.google.android.material.navigation.NavigationView;
+
+public class MenuMainGestorActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private String email;
+    private FragmentManager fragmentManager;
+    public static final String EMAIL = "EMAIL", SHARED_FILE = "DADOS_USER", OPERACAO = "OPERACAO";
+    public static final int EDIT = 10, ADD = 20, DELETE = 30;
+    private final int CAMERA_PERMISSION_CODE = 112;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE);
+        setContentView(R.layout.activity_menu_main_gestor);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.app_open,R.string.app_close);
+        toggle.syncState();
+        drawer.addDrawerListener(toggle);
+
+        carregarCabecalho();
+        navigationView.setNavigationItemSelectedListener(this);
+        fragmentManager = getSupportFragmentManager();
+        carregarFragmentoInicial();
+
+    }
+
+    private boolean carregarFragmentoInicial() {
+        Menu menu = navigationView.getMenu();
+        MenuItem item = menu.getItem(0);
+        item.setChecked(true);
+        return onNavigationItemSelected(item);
+    }
+
+    private void carregarCabecalho() {
+        email = getIntent().getStringExtra(EMAIL);
+        SharedPreferences sharedInfoUser = getSharedPreferences(SHARED_FILE, Context.MODE_PRIVATE);
+        if(email != null){
+            SharedPreferences.Editor editor = sharedInfoUser.edit();
+            editor.putString(EMAIL, email);
+            editor.apply();
+        } else
+            email = sharedInfoUser.getString(EMAIL, "Sem Email");
+
+
+        View hview = navigationView.getHeaderView(0);
+        TextView tvEmail = hview.findViewById(R.id.tvEmail);
+        tvEmail.setText(email);
+        
+    }
+
+
+    private void logout() {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        if (item.getItemId() == R.id.utilizador) {
+            fragment = new UtilizadorFragment();
+            setTitle(item.getTitle());
+        } else if (item.getItemId() == R.id.reservas) {
+            fragment = new ListaReservasGestorFragment();
+            setTitle(item.getTitle());
+        } else if (item.getItemId() == R.id.camara) {
+            fragment = new Camara_GestorFragment();
+            setTitle(item.getTitle());
+        } else if (item.getItemId() == R.id.logout) {
+            logout();
+        }
+
+        if (fragment != null) {
+            fragmentManager.beginTransaction().replace(R.id.contentFragment, fragment).commit();
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
+
+    private void checkPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(MenuMainGestorActivity.this,permission) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(MenuMainGestorActivity.this, new String[]{permission}, requestCode);
+        } else{
+            Toast.makeText(MenuMainGestorActivity.this,"Permissâo Garantida", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_CODE)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
